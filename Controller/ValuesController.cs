447e0c8aafa5d1a9dbd219e8ff8beb53f06a1b1f
Ranjithkumar T <61627002+RanjithKumart261@users.noreply.github.com>
2022-03-23 11:31:21 +0530

    second commit

---
+++ Controller/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Solution.Models;
+using System;
+using Solution.Repositories;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+
+namespace Solution.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //private readonly ILogger<ValuesController> _logger;
+        private readonly UserRepository repository;
+        //private readonly EmployeeRepository employeeRepository;
+
+        public ValuesController()
+        {
+            repository = new UserRepository("User Id=postgres;Password=TRIVENI@1;Host=localhost;Database=Solution1");
+            //employeeRepository = new EmployeeRepository("User Id=postgres;Password=TRIVENI@1;Host=localhost;Database=Solution");
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            var result = repository.GetAll();
+            return Ok(result.Select(x => new User
+            {
+                Email = x.Email,
+                Id = x.Id,
+                IsUpdated   = x.IsUpdated,
+                Password = x.Password
+            }));
+        }
+
+   
+        [HttpPut]
+      
+        public ActionResult<User> Put(User user)
+        {
+            repository.Update(user);
+            return user;
+        }
+
+        [HttpPost]
+        public ActionResult<User> Post(User user)
+        {
+            repository.Create(user);
+            return user;
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            repository.Delete(id);
+            return Ok();
+        }
+
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,33 @@
+﻿using FluentNHibernate.Mapping;
+using System.Text.Json.Serialization;
+
+namespace Solution.Models
+{
+    public class User
+    {
+        public virtual int Id { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string Password { get; set; }
+        public virtual bool? IsUpdated { get; set; }
+
+            
+    }
+
+    public class UserMapping : ClassMap<User>
+    {
+        public UserMapping()
+        {
+            Id(x => x.Id)
+                .GeneratedBy.Native()
+                .Column("Id");
+            Map(x => x.Email)
+                .Column("email");
+            Map(x => x.Password)
+                .Column("password");
+            Map(x => x.IsUpdated)
+                .Column("is_updated");
+            Table("Users");
+        }
+    }
+}
+
